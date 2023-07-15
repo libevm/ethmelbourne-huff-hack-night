a20d2e304528b61fd116be28b1a67e31d2da4a8f
Thomas WOSTYN <thomas.wostyn@gmail.com>
2023-07-15 16:45:45 +1000

    Special bounty

--- test/SimpleStorage.t.sol
+++ test/SimpleStorage.t.sol
@@ -9,8 +9,18 @@ using { create } for bytes;
 
 interface ISimpleStorage {
     function increment() external;
-    function setValue(uint256) external;
+    
     function getValue() external returns (uint256);
+    function setValue(uint256) external;
+    
+    function getString() external returns (string memory);
+    function setString(string memory) external;
+
+    function getArray() external returns (uint256[] memory);
+    function setArray(uint256[] memory) external;
+
+    function getMap(bytes32 key) external returns (bytes32);
+    function setMap(bytes32 key, bytes32 value) external;
 }
 
 contract SimpleStorageTest is Test {
@@ -41,4 +51,42 @@ contract SimpleStorageTest is Test {
         simpleStorage.increment();
         assertEq(simpleStorage.getValue(), 4);
     }
+
+    function testSimpleStorageString_e2e() public {
+        assertEq(simpleStorage.getString(), "");
+        simpleStorage.setString("Thomas");
+        assertEq(simpleStorage.getString(), "Thomas");
+        simpleStorage.setString("ethmelbourne");
+        assertEq(simpleStorage.getString(), "ethmelbourne");
+        simpleStorage.setString("working?");
+        assertEq(simpleStorage.getString(), "working?");
+     }
+
+    function testSimpleStorageArray_e2e() public {
+        uint256[] memory myArray = new uint256[](4);
+        myArray[0] = 9;
+        myArray[1] = 8;
+        myArray[2] = 7;
+        myArray[3] = 6;
+
+        simpleStorage.setArray(myArray);
+        assertEq(simpleStorage.getArray(), myArray);
+    }
+
+    function testSimpleStorageMap_e2e() public {
+        bytes32 key = "eth";
+        bytes32 value = "Melbourne";
+        simpleStorage.setMap(key, value);
+        assertEq(simpleStorage.getMap(key), value);
+
+        // Update for same key
+        value = "Test2";
+        simpleStorage.setMap(key, value);
+        assertEq(simpleStorage.getMap(key), value);
+
+        key = "newKey";
+        simpleStorage.setMap(key, value);
+        assertEq(simpleStorage.getMap(key), value);
+    }
+
 }
